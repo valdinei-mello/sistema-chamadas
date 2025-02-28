290f7b8598323629609657ce0e7251667c2de093
valdinei-mello <valdinei.mello.22@gmail.com>
2025-02-27 21:35:21 -0300

    Criou pagina de login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,36 @@
+import "./signin.css";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Logo do sistema" />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input
+                        type="text"
+                        placeholder="Seu e-mail"
+                        value={email}
+                        onChange={(e) => { setEmail(e.target.value) }} />
+                    <input
+                        type="password"
+                        placeholder="Sua senha"
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value) }} />
+                    <button type="submit" >Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
+        </div>
+    );
+}
