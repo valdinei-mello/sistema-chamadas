1d1aac6174d4da73c9eaf49a5b739d2fb4871478
valdinei-mello <valdinei.mello.22@gmail.com>
2025-03-03 11:31:51 -0300

    Cadastrando usuario pt2

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -2,12 +2,16 @@ import { useState, createContext, useEffect } from "react";
 import { auth, db } from "../services/firebaseConnection";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   function signIn(email, password) {
     console.log(email);
@@ -26,21 +30,28 @@ function AuthProvider({ children }) {
           nome: name,
           avatarUrl: null,
         }).then(() => {
-          setLoading(false);
           let data = {
             uid: uid,
             email: value.user.email,
             nome: name,
             avatarUrl: null,
           };
-
+          storegeUser(data);
+          setLoading(false);
           setUser(data);
+
+          navigate("/dashboard");
+          toast.success("Bem vindo(a) a plataforma!");
         });
       })
       .catch((error) => {
         console.log(error);
         setLoading(false);
       });
+  }
+
+  function storegeUser(data) {
+    localStorage.setItem("@ticketsPRO", JSON.stringify(data));
   }
 
   return (
